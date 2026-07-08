f666933c156584ea2a66dba6608ecb4936d7bf4d
Shoaib Khan <shoaib7878687@gmail.com>
2026-07-08 20:04:50 +0500

    updated changes

--- Gsap/src/App.jsx
+++ Gsap/src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
-
-
+import { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 function App() {
+  const box = useRef();
+
+  useGSAP(() => {
+    gsap.to(box.current, {
+      x: 300,
+      duration: 2,
+    });
+  });
 
   return (
-    <>
-     <h1>GSAP</h1>
-    </>
-  )
-}
 
-export default App
+    <div ref={box}>Hello GSAP</div>
+  )
+  }
+
+export default App;
