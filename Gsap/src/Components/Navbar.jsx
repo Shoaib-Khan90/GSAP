1e9aa67c80f5ce7cbe2f5620b93b1274ca04c6ee
Shoaib Khan <shoaib7878687@gmail.com>
2026-07-11 18:41:39 +0500

    updated changes

--- Gsap/src/Components/Navbar.jsx
+++ Gsap/src/Components/Navbar.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
 import './Navbar.css'
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 const Navbar = () => {
+
+  
+useGSAP(() => {
+  const tl = gsap.timeline();
+
+  tl.from(".child", {
+    y: -100,
+    opacity: 0,
+    duration: 1,
+    rotate: 20,
+    scale: 0.2,
+    ease: "power1.out",
+  });
+
+  tl.from(".Links li", {
+    x: 100,
+    opacity: 0,
+    duration: 1,
+    delay:0.4,
+    stagger: 0.2,
+    ease: "back.out(1.7)",
+  });
+});
+
+
   return (
     <div className='Header'>
       <div className='child'>
-      <ul>
+      <ul className='Links li'>
         <li> <Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/trainer">Trainer</Link></li>
